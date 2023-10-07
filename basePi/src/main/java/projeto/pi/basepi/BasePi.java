ccf490ecd4be3c25422367fe5992b086c50d5727
Eduardo Assis <124646021+eduziss@users.noreply.github.com>
2023-10-07 15:41:40 -0300

    Add files via upload

---
+++ basePi/src/main/java/projeto/pi/basepi/BasePi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto.pi.basepi;
+
+/**
+ *
+ * @author athyla
+ */
+public class BasePi {
+    
+    
+    Login logar = new Login();
+    
+
+    
+   
+    public static void main(String[] args) {
+    }
+}
---
+++ basePi/src/main/java/projeto/pi/basepi/Verficacao.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto.pi.basepi;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author dudu2
+ */
+public class Verficacao {
+    
+    public void verificacaoTexto(JTextField txt) {
+
+        try {
+
+            if (txt.getText().trim().equals("")) {
+                throw new IllegalArgumentException();
+            }
+
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, "Digite um valor para o campo " + txt.getName());
+            txt.setBackground(Color.red);
+        }
+
+    }
+
+    public void verificacaoNumeros(JTextField txtN) {
+
+        String id = txtN.getText();
+
+        if (!txtN.getText().trim().equals("")) {
+            try {
+
+                Integer.valueOf(id);
+
+            } catch (Exception e) {
+                //Escreva aqui a mensagem para o usuário
+                JOptionPane.showMessageDialog(null, "Campo número permite somente números!");
+                txtN.setBackground(Color.red);
+            }
+
+        }
+
+    }
+    
+}
